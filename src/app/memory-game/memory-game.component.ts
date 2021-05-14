e8b8c699843d6b88568e6260e34c9c4e2a3c2978
Paj Gabor <shockdest@gmail.com>
2021-05-14 11:47:57 +0200

    card flip works well, for now only at hover

--- src/app/memory-game/memory-game.component.ts
+++ src/app/memory-game/memory-game.component.ts
@@ -7,97 +7,102 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class MemoryGameComponent implements OnInit {
   itemlist = [];
-  randomList =[];
-  defaultCards=[];
-  isGame =false;
+  randomList = [];
+  defaultCards = [];
+  isGame = false;
 
 
   constructor() {
     this.defaultCards = [
       {
-        type:'angular',img:'../../assets/cards/angular.png'
+        type: 'angular', img: '../../assets/cards/angular.png'
       },
       {
-        type:'d3',img:'../../assets/cards/d3.png'
+        type: 'd3', img: '../../assets/cards/d3.png'
       },
       {
-        type:'jenkins',img:'../../assets/cards/jenkings.png'
+        type: 'jenkins', img: '../../assets/cards/jenkings.png'
       },
       {
-        type:'postcss',img:'../../assets/cards/postcss.png'
+        type: 'postcss', img: '../../assets/cards/postcss.png'
       },
       {
-        type:'react',img:'../../assets/cards/react.png'
+        type: 'react', img: '../../assets/cards/react.png'
       },
       {
-        type:'redux',img:'../../assets/cards/redux.png'
+        type: 'redux', img: '../../assets/cards/redux.png'
       },
       {
-        type:'sass',img:'../../assets/cards/sass.png'
+        type: 'sass', img: '../../assets/cards/sass.png'
       },
       {
-        type:'splendex',img:'../../assets/cards/splendex.png'
+        type: 'splendex', img: '../../assets/cards/splendex.png'
       },
       {
-        type:'ts',img:'../../assets/cards/ts.png'
+        type: 'ts', img: '../../assets/cards/ts.png'
       },
       {
-        type:'webpack',img:'../../assets/cards/webpack.png'
+        type: 'webpack', img: '../../assets/cards/webpack.png'
       },
-      ];
+    ];
   }
 
   ngOnInit(): void {
 
-    for(let i = 3;i<=10;i++){
+    for (let i = 3; i <= 10; i++) {
       this.cardNumberSelectArray.push(i);
-      }
-      // console.log(this.cardNumberSelectArray);
+    }
+    // console.log(this.cardNumberSelectArray);
+
+    let cards = Array.from(document.getElementsByClassName('card'));
+
   }
+
 
   getRandomArbitrary(min, max, iteration) {
     return Math.floor(Math.random() * (max - min) + min);
   }
 
-  getSelectValue(){
+  getSelectValue() {
     localStorage.setItem("SelectValue", this.optionValue.nativeElement.value);
-    this.isGame =true;
+    this.isGame = true;
     //console.log(this.optionValue.nativeElement.value);
   }
 
 
- /*  getRandomIndexes(){ //for 
-    this.getRandomArbitrary(3, 10, localStorage.getItem("SelectedValue")) // kell még egy tempArray - ahova mennek a random számok.
-    console.log(this.getRandomArbitrary(3, 10, localStorage.getItem("SelectedValue")));
-} */
+  /*  getRandomIndexes(){ //for 
+     this.getRandomArbitrary(3, 10, localStorage.getItem("SelectedValue")) // kell még egy tempArray - ahova mennek a random számok.
+     console.log(this.getRandomArbitrary(3, 10, localStorage.getItem("SelectedValue")));
+ } */
 
   // use localstorage
-  //create service /cardService  - Obs fgv, Megadta-e a user a cardok számát, ha igen megadja
-  
-  createCardDeck(){
-    //for megduplázza az adott arrayek genyóját
+
+  createCardDeck() {
+    // kell megduplázás
     console.log(localStorage.getItem("SelectValue"));
-    for(let i=0; i<=JSON.parse(localStorage.getItem("SelectValue"))-1;i++){
+    for (let i = 0; i <= JSON.parse(localStorage.getItem("SelectValue")) - 1; i++) {
       let newElements = Math.floor(Math.random() * (10 - 3) + 3);
       this.randomList.push(newElements);
       this.itemlist.push(this.defaultCards[newElements]);
       this.itemlist.push(this.defaultCards[newElements]);
-      this.itemlist.sort(() => Math.random() - 0.5);
-      
+
     }
+    this.itemlist.sort(() => Math.random() - 0.5);
+
     console.log(this.randomList);
     console.log(this.itemlist);
-    this.isGame =true;
-    
+    this.isGame = true;
+
   }
 
-  startButton(){
+  startButton() {
     this.getSelectValue();
     this.createCardDeck();
   }
 
   cardNumberSelectArray = [];
-  @ViewChild("optionValue") optionValue:ElementRef;
+  @ViewChild("optionValue") optionValue: ElementRef;
+
 
 
 }
